1d68aff665e5c84817a9f979d9431c6f02c70dae
ljc0211 <757818798@qq.com>
2019-05-21 00:12:57 +0800

    Signed-off-by: ljc0211 <757818798@qq.com>

---
+++ 14.java
@@ -0,0 +1,15 @@
+package com.ljc;
+
+import com.ljc.ASD;
+
+class Solution {
+  private ASD a1;
+  public Solution() {
+    a1 = new ASD();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().a1.m1);
+  }
+
+}
---
+++ Map.java
@@ -0,0 +1,18 @@
+package com.ljc;
+
+import java.util.Map;
+import java.util.HashMap;
+
+class ASD {
+        public Map<String, Integer> m1 = new HashMap<>();
+
+        public ASD() {
+          m1.put("I",1);
+          m1.put("V",5);
+          m1.put("X",10);
+          m1.put("L",50);
+          m1.put("C",100);
+          m1.put("D",500);
+          m1.put("M",1000);
+        }
+}
