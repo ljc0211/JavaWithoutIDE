f5e234337b2f946287cfd0d92bcdaea732da03ee
ljc0211 <38723534+ljc0211@users.noreply.github.com>
2019-06-01 23:10:36 +0800

    模拟哈希表？

--- simple.java
+++ simple.java
@@ -1,58 +1,65 @@
-package template;
+// ¹şÏ£±íËã·¨ËÑË÷ËùÓĞÄÜ´Õ³ÉÖ¸¶¨ºÍµÄÕûĞÎÊı×éºÏ¡£
+package com.threesum_15;
 import java.util.*;
 
-
 class Task {
-	private String[] ss_exp = {"fwief", "fwiefei", "fwiefee"};
-  private int[] nn_exp = {2, -1, 4, 0, 0, 0, 1, 3, -3};
-	public Task() {
 
-	}
+        private int[] nn_exp = {5, 4, 2, 1, 3, 5, 1, 0, 7};
 
-	// æ‰“å°Intå‹æ•°ç»„ã€‚
-	private void printIntArray(int[] nn_exp) {
-		for (int n : nn_exp) {
-			System.out.println(n);
-		}
-	}
+        // TODO: ÆÕÍ¨Ëã·¨ËÑË÷
+        private List<List<Integer> > normalSearch(int[] nums) {
+                List<List<Integer> > llist_n = new ArrayList<>();
 
-  public List<List<Integer>> demo(int[] nn_exp) {
-    List<List<Integer>> llist = new ArrayList<>();
+                // Arrays.sort(nums);
 
-		for (int i = 0; i < nn_exp.length - 2; i++) {
-			for (int j = i + 1; j < nn_exp.length - 1; j++) {
-				for (int k = j + 1; k < nn_exp.length; k++) {
-					if ((nn_exp[i] + nn_exp[j] + nn_exp[k]) == 0) {
-						List<Integer> list = new ArrayList<>();
-						list.add(nn_exp[i]);
-						list.add(nn_exp[j]);
-						list.add(nn_exp[k]);
-						llist.add(list);
-					}
-				}
-			}
-		}
+                for (int i = 0; i < nums.length - 1; i++) {
+                        for (int j = i + 1; j < nums.length; j++) {
+                                if ((nums[i] + nums[j]) == 7) {
+                                        List<Integer> list_n = new ArrayList<>();
+                                        list_n.add(nums[i]);
+                                        list_n.add(nums[j]);
+                                        llist_n.add(list_n);
+                                }
+                        }
+                }
 
-		HashSet<List<Integer>> hsllist = new HashSet<>(llist);
-	   List<List<Integer>> llistCopy = new ArrayList<>(hsllist);
+                HashSet<List<Integer> > hlist_n = new HashSet<>(llist_n);
+                List<List<Integer> > llist_n2 = new ArrayList<>(hlist_n);
 
+                return llist_n2;
+        }
 
-// åªæ˜¯æµ‹è¯• return çš„åŸºæœ¬è¿”å›åŠŸèƒ½ã€‚
-		// for (int n : nn_exp) {
-		// 	list.add(n);
-		// }
-		//
-		// llist.add(list);
-		// llist.add(list);
+        // TODO£º¹şÏ£±íËã·¨ËÑË÷
+        private List<List<Integer> > hashSearch(int[] nums) {
 
-		return llistCopy;
-  }
+                List<List<Integer> > llist_n = new ArrayList<>();
 
-//  static é‡Œé¢æ²¡æœ‰ä»»ä½•thisæŒ‡é’ˆï¼Œæ–¹æ³•ä½“æ— æ³•è¯†åˆ«è¿™ä¸ªå˜é‡åˆ°åº•å±äºå“ªä¸€ä¸ªç±»ã€‚
-	public static void main(String[] args) {
-    Task t1 = new Task();
+                Arrays.sort(nums);
 
-		System.out.println(t1.demo(t1.nn_exp));
+                int[] expForHash = new int[nums.length];
 
-	}
+                Arrays.fill(expForHash, 9999);
+
+                for (int i = 0; i < nums.length; i++) {
+                        if (expForHash[nums[i]] != 9999) {
+                                List<Integer> list_n = new ArrayList<>();
+                                list_n.add(nums[i]);
+                                list_n.add(expForHash[nums[i]]);
+                                llist_n.add(list_n);
+                        }
+                        else {
+                                expForHash[8 - nums[i]] = nums[i];
+                        }
+                }
+
+                HashSet<List<Integer> > hlist_n = new HashSet<>(llist_n);
+                List<List<Integer> > llist_n2 = new ArrayList<>(hlist_n);
+
+                return llist_n2;
+        }
+
+        public static void main(String[] args) {
+                Task t1 = new Task();
+                System.out.println(t1.hashSearch(t1.nn_exp));
+        }
 }
